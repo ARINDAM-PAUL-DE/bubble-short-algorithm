86c571538e9f8baec182e6f6311da15001cde278
Arindam Paul <76865623+ARINDAM-PAUL-DE@users.noreply.github.com>
2022-01-14 00:56:06 +0530

    Create bubble short.cpp

---
+++ bubble short.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  for(int i=1;i<n;i++){
+    //for round 1 to n
+    for(int j=0;j<n-1;j++){
+      if(arr[i]>arr[j+1]){
+        swap[j] , arr[j+1];
+      }
+    }
+  }
